87a6f1f0b3a8e5b67efe287b212b090d7a60d0b7
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-10-18 15:21:02 -0700

    fixed status

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -19,7 +19,7 @@ app.get("/:username", async (req, res) => {
         if(!user) {
             return res.status(404).json({ error: "User not found" });
         }
-        res.json(user);
+        res.status(200).json(user);
     } catch(err) {
         res.status(400).json({error: err.message});
     }
