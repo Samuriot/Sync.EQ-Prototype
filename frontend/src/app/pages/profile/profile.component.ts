1624a73805990b35d16ba9a888ae3c210240cdd8
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-10-27 12:43:54 -0700

    Fixed User Interface to correctly store info from API call to backend

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SynceqHeader } from '../../components/homeHeader/homeHeader.component';
 import { User } from '../../models/users';
 import { UserService } from '../../services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -14,13 +15,18 @@ export class ProfilePage {
   user?: User;
   isLoading? = true;
   error = '';
-  @Input() username!: string;
+  username!: string;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+              private route : ActivatedRoute
+  ) {}
 
   ngOnInit(){
+    this.username = this.route.snapshot.paramMap.get('username') ?? '';
     if (this.username) {
       this.fetchUser();
+    } else {
+      console.log("nothing found");
     }
   }
 
@@ -28,6 +34,7 @@ export class ProfilePage {
     this.isLoading = true;
     this.userService.getProfile(this.username).subscribe({
       next: (data) => {
+        console.log(data);
         this.user = data;
         this.isLoading = false;
       },
