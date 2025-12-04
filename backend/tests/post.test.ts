2c5eead5367e47beb8aa53ea6302500ec36a844c
Jomikael Ruiz <92127962+Samuriot@users.noreply.github.com>
2025-12-04 13:36:57 -0800

    Tests: Fixing Jest Bug (#76)
    * testing CI/CD on Azure
    
    * updated directory names
    
    * fixed backend dockerfile
    
    * fixed environment.ts file creation
    
    * updated jest testing to account for backend changes
    
    * added .js extensions to files
    
    * added testing to docker image build
    
    * 201 post request testing
    
    * Removed commenting to  make it easier to read

---
+++ backend/tests/post.test.ts
@@ -0,0 +1,70 @@
+jest.mock('../models/user.js', () => ({
+    __esModule: true,
+    default: {
+        create: jest.fn(),
+        find: jest.fn(),
+        findOne: jest.fn(),
+        findOneAndUpdate: jest.fn(),
+    }
+}));
+import User from '../models/user.js';
+import { createUser } from '../controllers/userController.js';
+import { Request, Response } from 'express';
+
+const mockUser = {
+    auth0id: "auth0|206",
+    username: "mockUser",
+    artistAlias: "Test Artist",
+    professions: ["Producer", "Composer"],
+    genres: ["Electronic", "Ambient"],
+    biography: "This is a test user for unit testing.",
+    phone_num: "123-456-7890",
+    email: "hola@gmail.com",
+    daws: ["Ableton Live", "FL Studio"],
+    external_visits: ["http://example.com/visitme", ],
+    profile_pic: "",
+    social_media: {
+        "Instagram": "http://instagram.com/testuser1",
+        "Twitter": "http://twitter.com/testuser1"
+    }
+}
+
+describe('Post Requests', () => {
+    let mockRequest: Partial<Request>;
+    let mockResponse: Partial<Response>;
+    let responseObject: any;
+    beforeEach(() => {
+        mockRequest = {
+            body: mockUser
+        };
+        responseObject = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis(),
+            send: jest.fn().mockReturnThis(),
+        }
+        mockResponse = responseObject;
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('create user route should return 201 status', () => {
+        test("successful createUser request", async () => {
+            // Arrange
+            (User.create as jest.Mock).mockResolvedValue(mockUser);
+            mockRequest = {
+                body: mockUser
+            }
+
+            // Act
+            await createUser(mockRequest as Request, mockResponse as Response);
+
+            // Assert
+            expect(User.create).toHaveBeenCalledWith(mockUser);
+            expect(User.create).toHaveBeenCalledTimes(1);
+            expect(responseObject.status).toHaveBeenCalledWith(201);
+            expect(responseObject.json).toHaveBeenCalledWith(mockUser);
+        })
+    });
+});
